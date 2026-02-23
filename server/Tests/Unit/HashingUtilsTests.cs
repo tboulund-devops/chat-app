22f31993cbb6bacf4ce590f74dab6b620e7644f3
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2026-02-23 16:32:30 +0100

    pipeline debug and testing

--- server/Tests/Unit/HashingUtilsTests.cs
+++ server/Tests/Unit/HashingUtilsTests.cs
@@ -38,7 +38,7 @@ public class HashingUtilsTests
     public Task ShouldFailPasswordHashVerification()
     {
         // Arrange
-        const string wrongPassword = "veryWrongPassword";
+        const string wrongPassword = "WrongPassword";
         _hashingUtils.CreatePasswordHash("correctPassword", out var hash);
         
         // Act & Assert
