4b1a1fbfa31e92338a331d5467cc7d6f928119fd
Kamil Karwacki <kamil.karwacki65@gmail.com>
2026-02-24 16:37:52 +0100

    Pipeline check

--- server/Tests/Unit/HashingUtilsTests.cs
+++ server/Tests/Unit/HashingUtilsTests.cs
@@ -38,7 +38,7 @@ public class HashingUtilsTests
     public Task ShouldFailPasswordHashVerification()
     {
         // Arrange
-        const string wrongPassword = "W_rongPassword";
+        const string wrongPassword = "WrongPassword";
         _hashingUtils.CreatePasswordHash("correctPassword", out var hash);
         
         // Act & Assert
