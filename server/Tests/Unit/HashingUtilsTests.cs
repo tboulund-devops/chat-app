be55b03a80a377b0ba7feab507c2b487042ad24a
Kamil Karwacki <kamil.karwacki65@gmail.com>
2026-02-24 19:16:10 +0100

    pipline check

--- server/Tests/Unit/HashingUtilsTests.cs
+++ server/Tests/Unit/HashingUtilsTests.cs
@@ -38,7 +38,7 @@ public class HashingUtilsTests
     public Task ShouldFailPasswordHashVerification()
     {
         // Arrange
-        const string wrongPassword = "WrongPassword";
+        const string wrongPassword = "Wro_ngPassword";
         _hashingUtils.CreatePasswordHash("correctPassword", out var hash);
         
         // Act & Assert
